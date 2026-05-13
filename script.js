4e727e9c1447f6bd5fa57e105af9b3b41db44d8c
Lynn Chaker <lchaker921@tamu.edu>
2026-05-13 14:39:50 +0000

    New L'Oreal Smart Routine and Product Advisor website added

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ document.addEventListener('DOMContentLoaded', function() {
             description: 
               "A website built as an interactive NASA Space Explorer web application that pulls real astronomy images, videos, descriptions, and other metadata directly from NASA's APOD APIs. The site features dynamic content loading, allowing users to explore a past collection of space related media. It features date-based image retrieval, a responsive UI design, and accessibility features such as light and dark modes for accessibility. It is built with HTML, CSS, and JavaScript, and incorporates front-end development and creative technical execution to provide an engaging user experience while showcasing NASA's space imagery.",
             link: 'https://lynncgh22.github.io/07-nasa-space-explorer/?v=timestamp'
+        },
+        'Smart-Routine-&-Product-Advisor': {
+          title: "L'oreal Smart Routine & Product Advisor",
+          description:
+            "A website built based on the L'oreal brand that incorporates L'oreal products and allows users to select products that fall into different categories, such as haiorcare, makeup, and moisturizers and treatments. The site features a checklist and searchbar to filter for specific products, along with options to generate a unique routine for the user and clear selection. In addition, the user can ask follow-up questions related to facial and hair care. The website is built with HTML, CSS, and Javascript, and incorporates front-end development with light and dark modes as well as a translator feature for accessibility.",
+          link: 'https://lynncgh22.github.io/09-prj-loreal-routine-builder/'
         }
 
     };
